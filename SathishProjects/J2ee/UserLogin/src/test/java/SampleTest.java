3b2a16e54a332fdffc6fd9afc0445673ff621ae1
SunSat <sundar.sat84@gmail.com>
2017-10-15 01:30:13 +0530

    Committing Inital Structure for every one.

---
+++ SathishProjects/J2ee/UserLogin/src/test/java/SampleTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by anitha on 10/14/2017.
+ */
+public class SampleTest {
+}
---
+++ SathishProjects/Java/HackerRank/src/main/java/com/sunsat/sathish/java/hackerrank/App.java
@@ -0,0 +1,13 @@
+package com.sunsat.sathish.java.hackerrank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
